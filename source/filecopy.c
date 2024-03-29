8c00eb33774ec9165cd83e8418a200ea1220e6c1
CyberspyUA <itfcitcyberspecialist@gmail.com>
2024-03-29 12:20:55 +0200

    Project comleted! All necessary features added. Executable file added.

--- source/filecopy.c
+++ source/filecopy.c
@@ -8,5 +8,53 @@
 const int bufferSize = 10000;
 bool copyFile(const char* sourceFileName, const char* destinationFileName)
 {
-    return true;
+    int sourceFolder, destinationFolder;
+    off_t offset = 0;
+    char buffer[bufferSize];
+    ssize_t bytesRead, bytesWritten;
+    struct stat statisticsSource;
+
+    // Open source file
+    sourceFolder = open(sourceFileName, O_RDONLY);
+    if (sourceFolder == -1) 
+    {
+        perror("Error opening source file");
+        return false;
+    }
+
+    // Get source file stats
+    if (fstat(sourceFolder, &statisticsSource) == -1)
+    {
+        perror("Error getting source file stats");
+        close(sourceFolder);
+        return false;
+    }
+
+    // Opening destination file
+    destinationFolder = open(destinationFileName, O_WRONLY | O_CREAT | O_TRUNC, statisticsSource.st_mode);
+    if (destinationFolder == -1) 
+    {
+        perror("Error opening destination file");
+        close(sourceFolder);
+        return false;
+    }
+
+    // Copying file using standard I/O functions
+    while ((bytesRead = read(sourceFolder, &buffer, sizeof(buffer))) > 0) 
+    {
+        bytesWritten = write(destinationFolder, &buffer, bytesRead);
+        if (bytesWritten != bytesRead) 
+        {
+            perror("Error writing to destination file");
+            close(sourceFolder);
+            close(destinationFolder);
+            return false;
+        }
+    }
+
+    // Close files
+    close(sourceFolder);
+    close(destinationFolder);
+
+    return true; // Copying successful
 }
