9ad3a7e00f5c39523240cefc2d22595b09901a8b
CyberspyUA <itfcitcyberspecialist@gmail.com>
2024-03-29 11:17:59 +0200

    filecopy.c in progress

--- source/filecopy.c
+++ source/filecopy.c
@@ -6,49 +6,7 @@
 #include <sys/stat.h>
 
 const int bufferSize = 10000;
-bool copyFile(const char* sourceFileName, const char* destinationFileName){
-    int sourceFolder, destinationFolder;
-    off_t offset = 0;
-    char buffer[bufferSize];
-    ssize_t bytesRead, bytesWritten;
-    struct stat statisticsSource;
-
-    // Open source file
-    sourceFolder = open(sourceFileName, O_RDONLY);
-    if (sourceFolder == -1) {
-        perror("Error opening source file");
-        return false;
-    }
-
-    // Get source file stats
-    if (fstat(sourceFolder, &statisticsSource) == -1) {
-        perror("Error getting source file stats");
-        close(sourceFolder);
-        return false;
-    }
-
-    // Open destination file
-    destinationFolder = open(destinationFileName, O_WRONLY | O_CREAT | O_TRUNC, statisticsSource.st_mode);
-    if (destinationFolder == -1) {
-        perror("Error opening destination file");
-        close(sourceFolder);
-        return false;
-    }
-
-    // Copy file using standard I/O functions
-    while ((bytesRead = read(sourceFolder, &buffer, sizeof(buffer))) > 0) {
-        bytesWritten = write(destinationFolder, &buffer, bytesRead);
-        if (bytesWritten != bytesRead) {
-            perror("Error writing to destination file");
-            close(sourceFolder);
-            close(destinationFolder);
-            return false;
-        }
-    }
-
-    // Close files
-    close(sourceFolder);
-    close(destinationFolder);
-
-    return true; // Copying successful
+bool copyFile(const char* sourceFileName, const char* destinationFileName)
+{
+    return true;
 }
